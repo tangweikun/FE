29081b225fe76fdbe950b59b481e194abd22cbb3
tangweikun <819573105@qq.com>
2019-11-04 21:07:21 +0800

    :sparkles: add binary search and lazy man

---
+++ Programming.md
@@ -0,0 +1,11 @@
+# Programming Question
+
+## EASY
+
+1. JS 手写二分搜索算法
+
+## MEDIUM
+
+## HARD
+
+1. 实现一个 Lazyman
---
+++ programming-easy/01-binary-search.js
@@ -0,0 +1,20 @@
+// 704
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let current = Math.floor((start + end) / 2);
+    if (arr[current] === target) return current;
+    if (arr[current] < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 8], 8));
