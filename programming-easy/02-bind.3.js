b81b7ffdafa7fb98918037bc2d029cb0381f9d3f
tangweikun <819573105@qq.com>
2019-11-14 11:20:59 +0800

    :sparkles: add bind

---
+++ programming-easy/02-bind.3.js
@@ -0,0 +1,44 @@
+// HELP:
+
+Function.prototype._bind = function(oThis) {
+  if (typeof this !== "function") {
+    // closest thing possible to the ECMAScript 5
+    // internal IsCallable function
+    throw new TypeError(
+      "Function.prototype.bind - what is trying to be bound is not callable"
+    );
+  }
+
+  var aArgs = Array.prototype.slice.call(arguments, 1),
+    fToBind = this,
+    fNOP = function() {},
+    fBound = function() {
+      return fToBind.apply(
+        this instanceof fNOP ? this : oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments))
+      );
+    };
+
+  if (this.prototype) {
+    // Function.prototype doesn't have a prototype property
+    fNOP.prototype = this.prototype;
+  }
+  fBound.prototype = new fNOP();
+
+  return fBound;
+};
+
+var module = {
+  x: 42,
+  getX: function() {
+    return this.x;
+  }
+};
+
+const unboundGetX = module.getX;
+console.log(unboundGetX()); // The function gets invoked at the global scope
+// expected output: undefined
+
+const boundGetX = unboundGetX._bind(module);
+console.log(boundGetX());
+// expected output: 42
