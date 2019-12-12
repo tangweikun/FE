73a88c9c1d7ca70331d4c44a10644cf8f95f4023
tangweikun <819573105@qq.com>
2019-12-12 10:56:36 +0800

    :sparkles: add instanceOf

--- Programming.md
+++ Programming.md
@@ -17,6 +17,7 @@
 4. 实现 call
 5. 实现 apply
 6. 实现 create
+7. instanceOf
 
 ## HARD
 
---
+++ programming-medium/07-instanceOf.js
@@ -0,0 +1,16 @@
+// 思路：右边变量的原型存在于左边变量的原型链上
+function instanceOf(left, right) {
+  const leftValue = left.__proto__;
+  const rightValue = right.prototype;
+
+  while (true) {
+    if (leftValue === null) {
+      return false;
+    }
+    if (leftValue === rightValue) {
+      return true;
+    }
+
+    leftValue = leftValue.__proto__;
+  }
+}
